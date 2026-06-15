ef6d769cff1740439539538731175c0ef98fd855
katness.chen <sbxew99@gmail.com>
2026-06-15 15:54:25 -0400

    Refactor site content and layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://vizino-studio.vercel.app',
+  site: 'https://well-maintained.vercel.app',
   output: 'static',
   integrations: [react()],
 });
